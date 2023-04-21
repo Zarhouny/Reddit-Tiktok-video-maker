1e9fe91d229be8611d73ab7453016c5b2fccdfc2
Zarhouny <131412325+Zarhouny@users.noreply.github.com>
2023-04-21 01:31:15 +0000

    Create info.py

---
+++ info.py
@@ -0,0 +1,11 @@
+import praw
+reddit = praw.Reddit(
+    client_id = 'bot ID',
+    client_secret = 'bot secret',
+    username = 'your reddit username',
+    password = "your reddit account's password",
+    user_agent = "your reddit account's name"
+)
+
+
+# DO NOT SHARE THIS INFO WITH ANYONE AND ABSOLUTELY DO NOT SHARE THIS FILE WITH ANYONE AFTER FILLING UP YOUR INFORMATION, I AM NOT RESPONSIBLE FOR ACCOUNTS STOLEN OR HACKED INTO BECAUSE OF THIS
