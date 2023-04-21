9d4fbd51445fb137587a31624d19e877b684621e
Zarhouny <131412325+Zarhouny@users.noreply.github.com>
2023-04-21 01:29:30 +0000

    Delete info.py

--- info.py
+++
@@ -1,8 +0,0 @@
-import praw
-reddit = praw.Reddit(
-    client_id = 'GWPn4ZO4R4n72nOsE4km3g',
-    client_secret = 'e7QPbo98yWbIuAXQuD-RWH4E0--bCg',
-    username = 'Liqwed',
-    password = 'moslimzarhouni',
-    user_agent = 'Liqwyd'
-)
